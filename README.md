29b817fc01f3e10f56e986516c87b789bbb0d29e
huonglarne <nguyenlanhuong.vietnam@gmail.com>
2021-01-06 11:35:34 +0700

    practical 1

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Students are expected to:
 Student Info
 =======================
 
-* Name: *YourNameHere*
-* ID: *YourIdHere*
-* Group ID: *Your Group ID here*
+* Name: Nguyen Lan Huong
+* ID: BI9-114
+* Group ID: 2
 
---
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -0,0 +1,47 @@
+package vn.edu.usth.weather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import android.util.Log;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+        Log.i("here", "create");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        setContentView(R.layout.activity_weather);
+        Log.i("here", "start");
+    }
+
+    protected void onResume() {
+        super.onResume();
+        setContentView(R.layout.activity_weather);
+        Log.i("here", "resume");
+    }
+
+    protected void onPause() {
+        super.onPause();
+        setContentView(R.layout.activity_weather);
+        Log.i("here", "pause");
+    }
+
+    protected void onStop() {
+        super.onStop();
+        setContentView(R.layout.activity_weather);
+        Log.i("here", "stop");
+    }
+
+    protected void onDestroy() {
+        super.onDestroy();
+        setContentView(R.layout.activity_weather);
+        Log.i("here", "destroy");
+    }
+}
