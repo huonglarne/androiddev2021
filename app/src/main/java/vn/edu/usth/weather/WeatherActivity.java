f32f05bb0c7f7c3150a54774469817e2a276d11f
Huong Larne <nguyenlanhuong.vietnam@gmail.com>
2021-01-18 20:35:20 +0700

    practical 7 8

--- app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,49 +1,77 @@
 package vn.edu.usth.weather;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 
 import android.util.Log;
 
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class WeatherActivity extends AppCompatActivity {
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
-        Log.i("here", "create");
 
-        ForecastFragment ff = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.forecast_frag, ff, null).commit();
+        viewPager = (ViewPager) findViewById(R.id.view_pager);
+        addTab(viewPager);
 
-        WeatherFragment wf = new WeatherFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.weather_frag, wf, null).commit();
-
+        tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("here", "start");
+    private void addTab(ViewPager viewPager)
+    {
+        ViewPagerAdapter vpa = new ViewPagerAdapter(getSupportFragmentManager());
+        vpa.addFragment(new WeatherAndForecastFragment(), "Hanoi");
+        vpa.addFragment(new WeatherAndForecastFragment(), "Again Hanoi");
+        vpa.addFragment(new WeatherAndForecastFragment(), "Also Hanoi");
+        viewPager.setAdapter(vpa);
     }
 
-    protected void onResume() {
-        super.onResume();
-        Log.i("here", "resume");
-    }
+    class ViewPagerAdapter extends FragmentPagerAdapter
+    {
+        private final List mFragmentList = new ArrayList<>();
+        private final List mFragmentTitleList = new ArrayList<>();
 
-    protected void onPause() {
-        super.onPause();
-        Log.i("here", "pause");
-    }
+        public ViewPagerAdapter(FragmentManager fman)
+        {
+            super(fman);
+        }
 
-    protected void onStop() {
-        super.onStop();
-        Log.i("here", "stop");
-    }
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return (Fragment) mFragmentList.get(position);
+        }
 
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("here", "destroy");
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+        }
+
+        public void addFragment(Fragment fragment, String title)
+        {
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return (CharSequence) mFragmentTitleList.get(position);
+        }
     }
 }
